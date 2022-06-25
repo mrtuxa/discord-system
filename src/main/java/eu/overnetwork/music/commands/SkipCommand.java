b87838b1bded26c4a2ed9925933df60de70cf055
tuxa <69870860+mrtuxa@users.noreply.github.com>
2022-06-25 02:19:50 +0200

    update

---
+++ src/main/java/eu/overnetwork/music/commands/SkipCommand.java
@@ -0,0 +1,27 @@
+package eu.overnetwork.music.commands;
+
+import eu.overnetwork.music.audio.AudioManager;
+import eu.overnetwork.music.base.ServerCommand;
+import org.javacord.api.entity.channel.ServerTextChannel;
+import org.javacord.api.entity.server.Server;
+import org.javacord.api.entity.user.User;
+import org.javacord.api.event.message.MessageCreateEvent;
+
+public class SkipCommand extends ServerCommand {
+
+    public SkipCommand() {
+        super("skip");
+    }
+
+    @Override
+    protected void runCommand(MessageCreateEvent event, Server server, ServerTextChannel channel, User user, String[] args) {
+        // Checks if there are any audio connection.
+        server.getAudioConnection().ifPresentOrElse(connection -> {
+
+            // If there is an audio connection then we skip the track.
+            AudioManager.get(server.getId()).scheduler.nextTrack();
+            event.getChannel().sendMessage("We have skipped the tracked");
+
+        }, () -> event.getChannel().sendMessage("The bot doesn't seem to be playing any music."));
+    }
+}
