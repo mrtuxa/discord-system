0f5f6f53fb57aab6f2ed682f9f646980730cb608
tuxa <69870860+mrtuxa@users.noreply.github.com>
2022-07-03 01:22:57 +0000

    Mass added Functions

--- src/main/java/eu/overnetwork/core/Main.java
+++ src/main/java/eu/overnetwork/core/Main.java
@@ -1,50 +1,83 @@
 package eu.overnetwork.core;
 
-import eu.overnetwork.additional.music.audio.PlayerManager;
-import eu.overnetwork.additional.music.commands.LeaveCommand;
-import eu.overnetwork.additional.music.commands.PlayCommand;
-import eu.overnetwork.additional.music.commands.SkipCommand;
-import eu.overnetwork.additional.music.commands.StopCommand;
-import eu.overnetwork.additional.reaction.RollenAdd;
-import eu.overnetwork.additional.reaction.RollenRemove;
-import eu.overnetwork.additional.reaction.VerifyAdd;
-import eu.overnetwork.additional.reaction.VerifyRemove;
-import eu.overnetwork.cfg.Settings;
-import eu.overnetwork.cmd.*;
-import eu.overnetwork.listeners.ServerVoice;
+import eu.overnetwork.cmd.utils.PingCommand;
+import eu.overnetwork.handler.cmd.CommandHandler;
+import eu.overnetwork.listeners.music.JoinVoiceListener;
+import eu.overnetwork.listeners.reaction.AddReaction;
+import eu.overnetwork.listeners.reaction.RemoveReaction;
+import eu.overnetwork.util.CommandRegister;
+import eu.overnetwork.util.database.models.DiscordServer;
+import eu.overnetwork.util.database.repositories.DiscordServerRepository;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.entity.server.Server;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Collection;
 
 public class Main {
+
+    public enum VoiceConnectionStatus {
+        /**
+         * State for successful audio connection
+         */
+        Successful,
+        /**
+         * State for unsuccessful audio connection
+         */
+        Unsuccessful,
+        /**
+         * State for a pre-existing audio connection
+         */
+        AlreadyConnected
+    }
+
+    public static String defaultGuildPrefix = "!";
+
+    public static String[] audioSources = {
+            "youtube",
+    };
+
+    @Autowired
+    private static CommandHandler commandHandler;
+    public static DiscordServerRepository discordServerRepository = null;
+
+    public Main(DiscordServerRepository discordServerRepo) {
+        discordServerRepository = discordServerRepo;
+    }
+
     public static void main(String[] args) {
+        String botToken = Dotenv.load().get("TOKEN");
 
-        PlayerManager.init();
-
-        Settings cfg = new Settings();
-        Dotenv dotenv = Dotenv.load();
         DiscordApi api = new DiscordApiBuilder()
-                .setToken(dotenv.get("TOKEN"))
+                .setToken(botToken)
                 .setAllIntents()
+                .setWaitForServersOnStartup(true)
+                .setWaitForUsersOnStartup(true)
                 .login()
                 .join();
+        System.out.println("Bot has been started");
 
+        // handling server entries in database
+       /* if (discordServerRepository.findAll().isEmpty()) {
+            System.out.println("Bot server data repository empty, initializing data repository...");
+            Collection<Server> servers = api.getServers();
+            for (Server server : servers) {
+                discordServerRepository.save(new DiscordServer(String.valueOf(server.getId()), defaultGuildPrefix));
+            }
+            System.out.println("Bot server data repo initialized");*/
+
+        CommandRegister.run(api, commandHandler);
+
+
+        api.addReactionAddListener(new AddReaction());
+        api.addReactionRemoveListener(new RemoveReaction());
+        api.addListener(new JoinVoiceListener());
         System.out.println("Connected as " + api.getYourself().getDiscriminatedName());
 
-        api.addServerVoiceChannelMemberLeaveListener(new ServerVoice());
-
-        api.addMessageCreateListener(new Verify());
-        api.addListener(new Ping());
-        api.addListener(new Rollen());
-        api.addListener(new Tags());
-        api.addListener(new Language());
-        api.addListener(new PlayCommand());
-        api.addListener(new SkipCommand());
-        api.addListener(new StopCommand());
-        api.addListener(new LeaveCommand());
-        api.addReactionAddListener(new RollenAdd());
-        api.addReactionRemoveListener(new RollenRemove());
-        api.addReactionAddListener(new VerifyAdd());
-        api.addReactionRemoveListener(new VerifyRemove());
     }
 }
